65ae8819ccd9aeef53864ec57830f8707ccc855d
Nehal Soni <nehal.soni@gmail.com>
2011-11-09 22:12:55 +0000

    dashboard additions

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -9,11 +9,15 @@ class DashboardController < BaseController
       
       #@date = params[:month] ? Date.parse(params[:month]) : Date.today
       @date = (params[:month] and params[:year]) ? Date.strptime("#{params[:month]} #{params[:year]}", '%m %Y') : Date.today
-      @invoices_for_month = @invoices.find_all{|item| item.invoice_date.month == @date.month and item.invoice_date.year == @date.year }
+      
+      
+      @invoices_for_year = @invoices.find_all{|item| item.invoice_date.year == @date.year }
+      @invoices_for_month = @invoices_for_year.find_all{|item| item.invoice_date.month == @date.month }
       
       @total_invoice = @invoices.find_all{|item| item.invoice_date.year == @date.year}.collect {|item_total| item_total.total_cost_inc_tax_delivery}.reduce(:+)  
     
-      @invoices_month = current_company.invoices.accessible_by(current_ability).group_by{|invoice| invoice.invoice_date.month} 
+      @invoices_year_breakdown = @invoices_for_year.group_by{|invoice| invoice.invoice_date.month} 
+      @invoices_month_breakdown = @invoices_for_month.group_by{|invoice| invoice.invoice_date.day} 
 
     respond_to do | format |
       format.html

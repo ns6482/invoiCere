a9680c78ae557bbd8beadd9ce4a18414b99788be
Nehal Soni <nehal.soni@gmail.com>
2012-03-11 21:46:08 +0000

    items ajax

--- public/javascripts/itemAutoComplete.js
+++ public/javascripts/itemAutoComplete.js
@@ -2,33 +2,55 @@ $(function() {
 	
 	var projects = [
 			{
-				value: "jquery",
-				label: "jQuery",
-				desc: "the write less, do more, JavaScript library",
-				icon: "jquery_32x32.png"
+				value: "test item",
+				item_description: "test item",
+				qty: "1",
+				cost: "10.50",
+				item_type: "Pack"
 			},
-			{
-				value: "jquery-ui",
-				label: "jQuery UI",
-				desc: "the official user interface library for jQuery",
-				icon: "jqueryui_32x32.png"
+			{				
+				value: "test item2",
+				item_description: "test item2",
+				qty: "2",
+				cost: "11.50",
+				item_type: "Single"
 			},
-			{
-				value: "sizzlejs",
-				label: "Sizzle JS",
-				desc: "a pure-JavaScript CSS selector engine",
-				icon: "sizzlejs_32x32.png"
+			{							
+				value: "test item3",
+				item_description: "test item3",
+				qty: "4",
+				cost: "12.50",
+				item_type: "Hours"			
 			}
 		];
 
 	
     var options = {
         source: projects,
-        minLength: 1
+        minLength: 0, 
+        select: function( event, ui ) {
+        		//alert(ui.item.desc);
+				val = $(this).attr('id');
+				val = "#"+val.replace("item_description", "");
+				
+				$(val+"item_description").val(ui.item.item_description);
+				$(val+"qty").val(ui.item.qty);
+				$(val+"cost").val(ui.item.cost);
+				$(val+"item_type").val(ui.item.item_type);
+				
+				
+				//item_description qty, cost, type
+				//$( "#project" ).val( ui.item.label );
+				//$( "#project-id" ).val( ui.item.value );
+				//$( "#project-description" ).html( ui.item.desc );
+				//$( "#project-icon" ).attr( "src", "images/" + ui.item.icon );
+
+				return false;
+			}
+
     };
 
     $("input.item_description").live("keydown.autocomplete", function() {
-        $(this).autocomplete(options);
-        //alert($(this.id));
+        $(this).autocomplete(options);        
     });
     });

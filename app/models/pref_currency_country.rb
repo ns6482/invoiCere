b97849915d03e33a6becd4a10c9b53fb18697792
Nehal Soni <nehal.soni@gmail.com>
2012-03-22 21:08:24 +0000

    preferences

--- app/models/pref_currency_country.rb
+++ app/models/pref_currency_country.rb
@@ -1,6 +1,22 @@
-class PrefCurrencyCountry < ActiveHash::Base
+class PrefCurrencyCountry <  ActiveHash::Base
   self.data = [
-    {:id => 1, :name => "US"},
-    {:id => 2, :name => "Canada"}
+    {:id => 3, :name =>"Euro Member Countries, Euro"},
+{:id => 2, :name =>"United Kingdom, Pounds"},
+{:id => 7, :name =>"Australia, Dollars"},
+    {:id => 5, :name =>"Canada, Dollars"},
+{:id => 38, :name =>"Switzerland, Francs"},
+{:id => 45, :name =>"Czech Republic, Koruny"},
+{:id => 6, :name =>"Denmark, Kroner"},
+{:id => 63, :name =>"Hong Kong, Dollars"},
+{:id => 67, :name =>"Hungary, Forint"},
+{:id => 8, :name =>"Israel, New Shekels"},
+{:id => 4, :name =>"Japan, Yen"},
+{:id => 104, :name =>"Mexico, Pesos"},
+{:id => 110, :name =>"Norway, Krone"},
+{:id => 12, :name =>"New Zealand, Dollars"},
+{:id => 118, :name =>"Poland, Zlotych"},
+{:id => 129, :name =>"Sweden, Kronor"},
+{:id => 130, :name =>"Singapore, Dollars"}
+
   ]
 end
